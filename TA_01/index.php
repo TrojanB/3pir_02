291e5eded3702946b0c25da16efa87fc531148f8
3pir02_k01 <qwertroya@gmail.com>
2022-11-16 09:15:32 +0100

    TA_01

---
+++ TA_01/index.php
@@ -0,0 +1,15 @@
+
+    <?php
+    //Trojan Bogacki 3pir
+        $panstwa = array(
+            "Polska" => "Warszawa",
+            "Norwegia" => "Oslo",
+            "Włochy" => "Rzym",
+            "Grecja" => "Ateny",
+            "Szwecja" => "Sztokholm"
+        );
+
+        print_r($panstwa);
+        echo "<br>";
+        echo " Stolicą Polski jest $panstwa[Polska]";
+    ?>
